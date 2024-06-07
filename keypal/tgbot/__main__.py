89814939f75f56a423e3a0c07cba306f700affe4
ilya_goncharov <s02210358@gse.cs.msu.ru>
2024-06-07 14:26:36 +0300

    REVISE: code style

--- keypal/tgbot/__main__.py
+++ keypal/tgbot/__main__.py
@@ -1,3 +1,4 @@
+"""Python telegram bot for Bitwarden."""
 import telebot
 from telebot import types
 from ..bitwarden import Bitwarden as bitwd
@@ -11,14 +12,24 @@ bw = None
 
 bot = telebot.TeleBot(TOKEN)
 
+
 @bot.callback_query_handler(func=lambda call: True)
 def callback(call):
+    """
+    Realize the installed functionality in response to a inline button press.
+
+    More information in below match-case data structure.
+
+    :param call: object, that contain information about callback.
+    """
     data = call.data.strip().lower()
 
     match data:
         case "registered":
+            # log in
             bot.register_next_step_handler(call.message, email_request)
         case "unregistered":
+            # registration
             markup = types.InlineKeyboardMarkup()
             markup.add(types.InlineKeyboardButton("Create account",
                                                   url="https://vault.bitwarden.com/#/register?layout=default"))
@@ -31,9 +42,11 @@ def callback(call):
             bot.send_message(call.message.chat.id, "Click the button below when you register your account to log in.",
                              reply_markup=markup_new)
         case "get":
+            # get password by key
             bot.send_message(call.message.chat.id, "Enter key of password")
             bot.register_next_step_handler(call.message, get_password)
         case "get_all":
+            # get all password keys
             data = bw.get_list_of_names()
             res = "List of password keys:\n"
 
@@ -42,12 +55,20 @@ def callback(call):
 
             bot.send_message(call.message.chat.id, res)
         case "log_out":
+            # log out from bitwarden account
             bw.delete()
             init(call.message)
 
 
 @bot.message_handler(commands=['start'])
 def init(message):
+    """
+    /start command handling.
+
+    Checks that the user has a bitwarden account.
+
+    :param message: contain information about last message
+    """
     markup = types.InlineKeyboardMarkup(row_width=2)
 
     btn_yes = types.InlineKeyboardButton("Yes", callback_data="registered")
@@ -58,13 +79,24 @@ def init(message):
     bot.send_message(message.chat.id, "Hello, its KeyPal - telegram bot wrapper for Bitwarden password manager.")
     bot.send_message(message.chat.id, "Do you already have a Bitwarden account?", reply_markup=markup)
 
+
 # authorization
 def email_request(message):
+    """
+    Request the user's email address.
+
+    :param message: contain information about last message
+    """
     bot.send_message(message.chat.id, "Please enter your email")
     bot.register_next_step_handler(message, password_request)
 
 
 def password_request(message):
+    """
+    Request the user's password.
+
+    :param message: contain information about last message
+    """
     global name
     name = message.text.strip()
     bot.send_message(message.chat.id, "Please enter your password")
@@ -72,9 +104,14 @@ def password_request(message):
 
 
 def authorization_check(message):
+    """
+    Check if the received email and password are correct.
+
+    :param message: contain information about last message
+    """
     global password
     global bw
-    
+
     password = message.text.strip()
 
     try:
@@ -88,6 +125,11 @@ def authorization_check(message):
 
 # bitwaren cli requests
 def get_password(message):
+    """
+    Obtain password by key.
+
+    :param message: contain information about last message
+    """
     key = message.text.strip()
 
     data = bw.get_information(key)
@@ -96,6 +138,11 @@ def get_password(message):
 
 
 def global_menu(message):
+    """
+    Send welcome message with main menu.
+
+    :param message: contain information about last message
+    """
     markup = types.InlineKeyboardMarkup()
 
     button_get = types.InlineKeyboardButton("Get password", callback_data="get")
@@ -117,5 +164,6 @@ def global_menu(message):
 
     bot.send_message(message.chat.id, 'Welcome', reply_markup=markup_keyboard)
     bot.send_message(message.chat.id, "KeyPal - your Bitwarden password client", reply_markup=markup)
+
 
 bot.infinity_polling()
