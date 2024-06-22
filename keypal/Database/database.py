1f7ca3a524d5bd23853c73386f4d091739bd687c
Igor <Igor.ig0207@mail.ru>
2024-06-22 14:09:55 +0300

    Add checking id in the database

--- keypal/Database/database.py
+++ keypal/Database/database.py
@@ -9,7 +9,17 @@ class Database:
         query=""" CREATE TABLE IF NOT EXISTS APIKEYS(id TEXT, client_id TEXT, client_secret TEXT) """
         self.cursor.execute(query)
 
- 
+    def check(self):
+        query1=f"""SELECT COUNT(*) FROM APIKEYS WHERE id='{self.id}'"""
+        self.cursor.execute(query1)
+        result=self.cursor.fetchone()[0]
+        if result !=0:
+            query2=f"""SELECT client_id,client_secret FROM APIKEYS WHERE id='{self.id}'"""
+            self.cursor.execute(query2)
+            return self.cursor.fetchone()
+        return ()
+    
+   
 
 
 
