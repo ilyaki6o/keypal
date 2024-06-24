de918dfe2a46e5e733ae0cd3688a1b809d952dae
Roman Danilov <romanosauce.code@gmail.com>
2024-06-24 14:20:45 +0300

    Change docstrings, add methods, make small changes
    Arghhh, too many changes in one commit. I am too lazy to break them
    into different commits with patches. I am sorry.

--- keypal/bitwarden/Bitwarden.py
+++ keypal/bitwarden/Bitwarden.py
@@ -1,29 +1,45 @@
+"""Module for interaction with Bitwarden cli."""
+
 import pexpect
 from typing import List
 import json
 
 
 class LoginError(Exception):
+    """Exception for handling login errors."""
+
     pass
 
 
 class BitwardenClient:
-    """A client for interacting with the Bitwarden password manager."""
+    """Class for interacting with the Bitwarden password manager."""
+
     def __init__(self, client_id: str = '', client_secret: str = '') -> None:
+        """Init BitwardenClient class with user's secret info."""
         self.client_id = client_id
         self.client_secret = client_secret
+        self.unlocked = False
 
     def check_exitstatus(self, exit_code, exception, message):
+        """
+        Check exit status of command.
+
+        If it is not null raise given exception with given message
+        """
         if exit_code:
             raise exception(message)
 
+    def add_session_key(self, cmd):
+        """Append session key to command."""
+        return f"{cmd} --session {self.session_key}"
+
     def login(self, client_id: str = '', client_secret: str = '') -> None:
         """
-        Log in to the Bitwarden API using the provided client ID and client secret.
+        Log in to the Bitwarden API using provided client ID and client secret.
 
-        :param client_id: The client ID for the Bitwarden API.
+        :param client_id: Client ID for the Bitwarden API.
         :type client_id: str
-        :param client_secret: The client secret for the Bitwarden API.
+        :param client_secret: Client secret for the Bitwarden API.
         :type client_secret: str
         """
         child = pexpect.spawn("bw login --apikey")
@@ -36,25 +52,25 @@ class BitwardenClient:
         self.check_exitstatus(child.exitstatus,
                               LoginError,
                               "Invalid client_id or client_secret")
+        self.unlocked = False
 
     def logout(self) -> None:
-        """
-        Log out of the Bitwarden API.
-        """
+        """Log out from the Bitwarden API."""
         child = pexpect.spawn("bw logout")
         child.expect(pexpect.EOF)
         child.close()
         self.check_exitstatus(child.exitstatus,
                               LoginError,
                               "You are not logged in.")
+        self.unlocked = False
 
     def unlock(self, password: str) -> str:
         """
-        Unlock the Bitwarden vault using the provided master password.
+        Unlock Bitwarden vault using provided master password.
 
-        :param password: The master password for the Bitwarden vault.
+        :param password: Master password for the Bitwarden vault.
         :type password: str
-        :return: The session key obtained after unlocking the vault.
+        :return: Session key obtained after unlocking the vault.
         :rtype: str
         """
         self.password = password
@@ -66,26 +82,38 @@ class BitwardenClient:
         self.check_exitstatus(child.exitstatus,
                               LoginError,
                               "You are not logged in.")
-        self.session_key = child.before.splitlines()[-1]
+        self.session_key = child.before.splitlines()[-1].decode()
+        self.unlocked = True
 
     def lock(self) -> None:
-        """
-        Lock the Bitwarden vault.
-        """
+        """Lock the Bitwarden vault."""
         child = pexpect.spawn("bw lock")
         child.expect(pexpect.EOF)
         child.close()
         self.check_exitstatus(child.exitstatus,
                               LoginError,
                               "You are not logged in.")
+        self.unlocked = False
+
+    def list_items(self):
+        """Get list of all items in Bitwarden vault."""
+        data = []
+        if self.unlocked:
+            cmd = self.add_session_key("bw list items")
+            child = pexpect.spawn(cmd)
+            raw_data = child.read().decode()
+            payload_start = raw_data.find('[{')
+            data = json.loads(raw_data[payload_start:])
+        return data 
+
 
     def search_items_with_uri(self, uri: str):
         """
         Search for Bitwarden items that have a URI starting with the provided URI.
 
-        :param uri: The URI to search for.
+        :param uri: URI to search for.
         :type uri: str
-        :return: A list of URIs that match the search criteria.
+        :return: List of URIs that match the search criteria.
         :rtype: list
         """
         with pexpect.spawn("bw list items") as child:
@@ -106,11 +134,13 @@ class BitwardenClient:
         pass
 
     def sync(self) -> None:
-        """
-        Synchronize the Bitwarden vault with the server.
-        """
+        """Synchronize Bitwarden vault."""
         child = pexpect.spawn("bw sync")
         child.expect(pexpect.EOF)
+        child.close()
+        self.check_exitstatus(child.exitstatus,
+                              LoginError,
+                              "You are not logged in.")
 
     def get_password(self, id: str):
         """
@@ -137,7 +167,7 @@ class BitwardenClient:
         """
         Delete the Bitwarden item with the provided ID.
 
-        :param id: The ID of the Bitwarden item to delete.
+        :param id: ID of the Bitwarden item to delete.
         :type id: str
         """
         if id in self.search_items_with_uri(id):
@@ -162,6 +192,6 @@ if __name__ == "__main__":
     # bw2 = BitwardenClient()
     # bw2.login("user.65ba2bf2-52e7-461f-8a60-b199007a8fcd", "4dhEts3hBIsCDVYm5WwGklJ8N7cGZ5")
     bw1.unlock("CROSBY878697")
+    print(bw1.list_items())
     bw1.lock()
-    bw1.logout()
     bw1.logout()
