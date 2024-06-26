4e5a25191da72d2fb065317a06603372d5cb6df3
Roman Danilov <romanosauce.code@gmail.com>
2024-06-26 11:28:08 +0300

    Add numbers to tests and change list_items test

--- keypal/bitwarden/test_bitwarden_client.py
+++ keypal/bitwarden/test_bitwarden_client.py
@@ -13,29 +13,27 @@ class TestBitwardenClient(unittest.TestCase):
         self.client.lock()
         self.client.logout()
 
-    def test_list_items(self):
-        data = self.client.list_items()[0]
-        self.assertEqual(data, {'passwordHistory': None, 'revisionDate': '2024-06-25T12:32:49.866Z',
-                                'creationDate': '2024-06-25T12:32:49.866Z', 'deletedDate': None, 'object': 'item',
-                                'id': 'cadfbea5-8a5c-4fe3-bc34-b19a00cec572', 'organizationId': None, 'folderId': None,
-                                'type': 1, 'reprompt': 0, 'name': 'HOHO', 'notes': None, 'favorite': False,
-                                'login': {'fido2Credentials': [], 'uris': [], 'username': 'MEME', 'password': 'OHMY',
-                                'totp': None, 'passwordRevisionDate': None}, 'collectionIds': []})
+    def test_1_list_items(self):
+        data = self.client.list_items()
+        id_set = {item['id'] for item in data}
+        self.assertTrue(id_set == {"cadfbea5-8a5c-4fe3-bc34-b19a00cec572", "7aeb48e7-57af-4a82-aec5-b19b007f1775"})
 
-    def test_get_password_by_id(self):
+    def test_2_get_password_by_id(self):
         id = 'cadfbea5-8a5c-4fe3-bc34-b19a00cec572'
         data = self.client.get_password_by_id(id)
         self.assertEqual(data, ('MEME', 'OHMY'))
 
-    def test_create_password(self):
+    def test_3_create_password(self):
         uri = 'google'
         username = 'ROMAN'
         password = 'BITWARDENFOREVER'
         new_password = self.client.create_password(uri, username, password)
+        id = new_password['id']
         data = self.client.list_items()
         self.assertEqual(data[-1]['login']['password'], new_password['login']['password'])
+        self.client.del_password_by_id(id)
 
-    def test_create_and_del_password(self):
+    def test_4_create_and_del_password(self):
         uri = 'yaho'
         username = 'ILYA'
         password = 'iLOVETGBOT'
